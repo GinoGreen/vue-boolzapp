50b0e74a54c494936420fa9f61d390381b5f600e
GinoGreen <luigiverde97@gmail.com>
2021-11-15 13:48:25 +0100

    chat list complete

---
+++ Milestone_1/js/script.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+   el: '#app',
+
+   data: {
+      contacts: [
+         {
+            name: 'Simone',
+            avatar: '_1',
+            visible: true,
+            messages: []
+         },
+         {
+            name: 'Mauro',
+            avatar: '_2',
+            visible: true,
+            messages: []
+         },
+         {
+            name: 'Gianluca',
+            avatar: '_3',
+            visible: true,
+            messages: []
+         },
+         {
+            name: 'Andrea',
+            avatar: '_4',
+            visible: true,
+            messages: []
+         },
+         {
+            name: 'Giuseppe',
+            avatar: '_5',
+            visible: true,
+            messages: []
+         },
+      ],
+   },
+});
